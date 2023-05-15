6e9a0689821bbafc0050c98fdef9a0f967393c3c
Victor Mihaita <43206741+victormihaita@users.noreply.github.com>
2023-05-15 11:53:49 +0300

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Victor Mihaita on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
